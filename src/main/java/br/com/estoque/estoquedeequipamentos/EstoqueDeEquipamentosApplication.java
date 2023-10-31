1f384981f230d9ace7495d0ca4ba2710d6ea1b69
ramonptz <89748744+ramonptz@users.noreply.github.com>
2023-10-31 20:05:07 -0300

    init-project
    inicio do projeto

---
+++ src/main/java/br/com/estoque/estoquedeequipamentos/EstoqueDeEquipamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.estoque.estoquedeequipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueDeEquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueDeEquipamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estoque/estoquedeequipamentos/EstoqueDeEquipamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoque.estoquedeequipamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueDeEquipamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
